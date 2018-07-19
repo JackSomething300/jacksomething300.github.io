8c33db47d97c4452577508220866e6e3da9cb7a8
JackSomething300 <wobwobber300@gmail.com>
2018-07-19 21:30:00 +0200

    Styling changes and fix nav menu

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $(document).delegate('.open', 'click', function(event){
         $(this).addClass('oppenned');
+        $('.cls').css("background", "white");
         event.stopPropagation();
     })
     $(document).delegate('body', 'click', function(event) {
@@ -8,6 +9,7 @@ $(document).ready(function() {
     })
     $(document).delegate('.cls', 'click', function(event){
         $('.open').removeClass('oppenned');
+        $('.cls').css("background", "#2c2b2b");
         event.stopPropagation();
     });
 });
